a0fb4bc93368b069642b1735b496af2fe70ecbcb
TAN-UJ05 <tanujjoshi669@gmail.com>
2026-09-07 14:13:51 +0530

    Create LeetCode_492.java

---
+++ Experiment_1/LeetCode_492.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] constructRectangle(int area) {
+        int wid =(int) Math.sqrt(area);
+        while (area % wid != 0)
+            wid--;
+        return new int[] { area / wid, wid };
+    }
+}
